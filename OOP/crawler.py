07bec4dc158cab23222f51115993a3fe47433281
Hossein Dashti <hosseindashti@gmail.com>
2021-02-19 17:51:18 +1000

    Property definition

--- OOP/crawler.py
+++ OOP/crawler.py
@@ -4,18 +4,27 @@ import re
 
 class Website:
     def __init__(self, url):
-        self.url = url
-        self.text = ""
+        self.__url = url
+        self.__text = ""
 
-    def get_text(self):
-        with urllib.request.urlopen(self.url) as req:
-            self.text = req.read().decode('utf-8')
-        return self.text
+    @property
+    def url(self):
+        return self.__url
 
-    def get_links(self):
+    @property
+    def text(self):
+        if self.__text == "":
+            with urllib.request.urlopen(self.url) as req:
+                self.__text = req.read().decode('utf-8')
+        return self.__text
+
+    @property
+    def links(self):
         urls = re.findall(r'href="(https?://.+?)"', self.text)
         return [Website(url) for url in urls]
 
+    def __repr__(self):
+        return f"WEBSITE[{self.url}]"
 
 
 
@@ -27,12 +36,15 @@ website = Website(url)
 
 # print the website texts
 #print(website.get_text())
-text = website.get_text()
+text = website.text
 
 
 # list the websites in the website
-list_of_websites = website.get_links()
+list_of_websites = website.links
 
 
 # show the text of the fifth website
-print(list_of_websites[5].get_text())
+print(list_of_websites[5].text)
+
+print(list_of_websites)
+
